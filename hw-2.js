0e5229e32635861cc4c080cc8da8065a570a1cd7
Slav <cimvol@internet.ru>
2023-02-19 15:02:37 +0400

    two lesson

---
+++ hw-2.js
@@ -0,0 +1,120 @@
+/* Exercise 1
+let a = 10;
+alert(a);
+ a = 20;
+alert(a);*/
+
+
+/*Exercise 2
+let firstIphone;
+firstIphone = '9 января 2007 года';
+alert(`First iphone was made ${firstIphone}`);*/
+
+
+/*Exercise 3
+let maiker = 'Брендан Эйх';
+alert(`js developer is ${maiker}`);*/
+
+
+/*Exercise 4
+let a = 10;
+let b = 2;
+alert(`Result summ, ${a + b} 
+Result subtraction, ${a - b}
+Result multiplication, ${a * b}
+Result division, ${a / b}` ); */
+
+
+/*Exercise 5
+let num = 2;
+result = Math.pow(num,5);
+alert(result);*/
+
+
+/*Exercise 6
+let a = 9;
+let b = 2;
+result = 9 % 2;
+alert(result);*/
+
+
+/*Exercise 7
+let num = 1;
+num += 5; 
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);*/
+
+
+/*Exercise 8
+let age = prompt("How old are you?");
+alert(`I am ${age} years.`);*/
+
+
+/*Exercise 9.0
+const user = {
+    name: "Slava",
+    age: 33,
+    isAdmin: false,
+}
+console.log(user);
+*/
+
+
+/*Exercise 9.1
+const user = {
+    name: "Slava",
+    age: 33,
+    isAdmin: false,
+}
+user.cityofresidence = 'Mockow';
+console.log(user);
+*/
+
+
+/*Exercise 9.2
+const user = {
+    name: "Slava",
+    age: 33,
+    isAdmin: false,
+    cityofresidence: 'Mockow',
+}
+user.age = 40;
+console.log(user);
+*/
+
+
+/*Exercise 9.3
+const user = {
+    name: "Slava",
+    age: 33,
+    isAdmin: false,
+    cityofresidence: 'Mockow',
+}
+delete user.cityofresidence;
+console.log(user);
+*/
+
+
+/*Exercise 9.4
+
+const user = {
+    name: "Slava",
+    age: 33,
+    isAdmin: false,
+    cityofresidence: 'Mockow',
+}
+let info = prompt('What information do you need?');
+alert(user[info]);
+*/
+
+
+/*Exercise 10
+
+let name = prompt('What is your name?');
+alert(`Hello, ${name}!`);
+*/
+

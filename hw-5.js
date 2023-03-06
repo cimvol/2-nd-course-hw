423c1b595695775cf895bbaf04d1af8fad6a263c
Slav <cimvol@internet.ru>
2023-03-06 12:20:09 +0400

    WorkHome5

---
+++ hw-5.js
@@ -0,0 +1,83 @@
+/*Exersice 1
+function compar(a, b) {
+    if (a > b) {
+    return b;
+ } else 
+    return a;
+}
+console.log(compar (4, 9));
+*/
+
+
+ /* Exersice 2 
+ function numbers(user) {
+     if (user % 2 === 0) {
+    alert ('число четное');
+     } else 
+        alert ('число нечетное!');
+    }
+ numbers(user = prompt('Enter any numbers'));
+*/
+
+
+/* Exersice 3 (3.1)
+/*function squareNumber (num) {
+    res = num * num;
+    console.log(res);
+}
+squareNumber(num = prompt ('Enter your namber'));*/
+
+ /* Exersice 3 (3.2)
+ function squareNumberReturn (num) {
+    return  num * num;
+}
+let res = squareNumberReturn(6);
+alert (res);
+*/
+
+
+
+/* Exersice 4 
+function test (age) {
+    if (age < 0) {
+alert('Wrong!');
+}
+else if (age <= 12) {
+    alert('Hello Friend!');
+}
+else if (age >= 13) {
+    alert('Wellcome!');
+}
+}
+test(age = prompt('How old are you?'));
+*/
+
+
+/*Exersice 5
+function twoNum(a, b) {
+   if (isNaN(a) == false && isNaN(b) == false) {
+    return a * b;
+   } else 
+    alert("Один из символов не число");
+}
+let result = twoNum(7, 6);
+console.log(result);
+*/
+
+
+/*Exersice 6
+function squareNumber(n) {
+    if (isNaN(n) == true) {
+        alert('The entered character is not a number!');
+    } else 
+    return n * n;
+}
+ let result = squareNumber(a = prompt('Enter your number n'));
+ alert(`Your namber in square = ${result}`);
+*/
+
+
+
+
+
+
